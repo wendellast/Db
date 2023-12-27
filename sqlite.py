2af3c4ed07366eb0c3f02204fe0b378e68703303
wendellast <wendelalves01a@hotmail.com>
2023-12-27 11:12:35 -0300

    first commit

---
+++ sqlite.py
@@ -0,0 +1,22 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_name = 'db.sqlite3'
+DB_file = ROOT_DIR / DB_name
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_file)
+cursor =  connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+     'id INTEGER PRIMARY KEY AUTOINCREMENT'   
+    ')'
+)
+
+
+
+cursor.close()
+connection.close()
